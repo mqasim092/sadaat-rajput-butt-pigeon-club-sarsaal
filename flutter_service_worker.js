f97c07e53dc61b12c346574e752c7bf266636bc4
mqasim092 <19014198-092@uog.edu.pk>
2025-09-25 14:01:01 -0700

    Fixed Total Time calculation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6dd86cf1d5969f22264f9836e2653446",
+"flutter_bootstrap.js": "ca6ffadb119e41c468a8595e1aee92d9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "d51a41e51f3b0499d7cff44fa1b7bdf6",
 "/": "d51a41e51f3b0499d7cff44fa1b7bdf6",
-"main.dart.js": "b18d200ba9126e34a3d1b3d79be57d19",
+"main.dart.js": "89f3a523f512abc34d4f559aeaf13018",
 "manifest.json": "5aa331c6ba1d07b950f742b75fedd1ff",
 "version.json": "5487be2f81ce0f4815a1753f6fc7a5c4"};
 // The application shell files that are downloaded before a service worker can
